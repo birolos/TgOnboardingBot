adb9bdeef2d9700d3aa2ec375058166e996680fe
birolos <mrbirolos@gmail.com>
2023-04-11 16:33:38 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,16 +5,14 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, InputFile,
 from aiogram.utils.exceptions import BotBlocked
 from random import randint
 import aiogram.utils.markdown as fmt
+import sqlite3
+from sqlite3 import Error
 
 bot = Bot(token="5854619199:AAFO2U5yfHrOm6_9wBZTfudLf_4dgkGizoM")  # Объект бота
 dp = Dispatcher(bot)  # Диспетчер для бота
 logging.basicConfig(level=logging.INFO)  # Включаем логирование, чтобы не пропустить важные сообщения
 floor = 0
 
-starttext1 = open("start.txt", 'r', encoding="utf8")
-starttext = starttext1.readline()
-starttext = starttext + starttext1.readline()
-misatoMap = starttext1.readline(3)
 @dp.message_handler(commands="start")
 async def start_comms(message: types.Message):
     buttons = [
@@ -24,10 +22,10 @@ async def start_comms(message: types.Message):
     ]
     keyboard = types.InlineKeyboardMarkup(row_width=2)
     keyboard.add(*buttons)
-    await message.answer(starttext, reply_markup=keyboard)
+    await message.answer("Добро пожаловать в Nerv! ", reply_markup=keyboard)
 
 @dp.callback_query_handler(text="+1")
-async def text_plus1 (call: types.CallbackQuery, floor=floor):
+async def text_plus1(call: types.CallbackQuery, floor=floor):
     floor = 1
     buttons = [
         types.InlineKeyboardButton(text="-1", callback_data="-1.1"),
@@ -36,34 +34,12 @@ async def text_plus1 (call: types.CallbackQuery, floor=floor):
     ]
     keyboard = types.InlineKeyboardMarkup(row_width=2)
     keyboard.add(*buttons)
-    await call.message.edit_text("Этаж "+ str(floor), reply_markup=keyboard)
+    await call.message.answer("Этаж "+ str(floor), reply_markup=keyboard)
 
-@dp.callback_query_handler(text="+1.1")
-async def text_plus1 (call: types.CallbackQuery, floor=floor):
-    floor = 2
-    buttons = [
-        types.InlineKeyboardButton(text="-1", callback_data="-1.2"),
-        types.InlineKeyboardButton(text="+1", callback_data="+1.2"),
-        types.InlineKeyboardButton(text="Подтвердить", callback_data="num_finish2")
-    ]
-    keyboard = types.InlineKeyboardMarkup(row_width=2)
-    keyboard.add(*buttons)
-    await call.message.edit_text("Этаж "+ str(floor), reply_markup=keyboard)
-
-@dp.callback_query_handler(text="+1.2")
-async def text_plus1 (call: types.CallbackQuery, floor=floor):
-    floor = 3
-    buttons = [
-        types.InlineKeyboardButton(text="-1", callback_data="-1.3"),
-        types.InlineKeyboardButton(text="Подтвердить", callback_data="num_finish3")
-    ]
-    keyboard = types.InlineKeyboardMarkup(row_width=2)
-    keyboard.add(*buttons)
-    await call.message.edit_text("Этаж "+ str(floor), reply_markup=keyboard)
 
 @dp.callback_query_handler(text="-1")
-async def text_plus1 (call: types.CallbackQuery, floor=floor):
-    floor = 0
+async def text_plus1(call: types.CallbackQuery, floor=floor):
+    floor =-1
     buttons = [
         types.InlineKeyboardButton(text="-1", callback_data="-1.2"),
         types.InlineKeyboardButton(text="+1", callback_data="+1.2"),
@@ -71,45 +47,72 @@ async def text_plus1 (call: types.CallbackQuery, floor=floor):
     ]
     keyboard = types.InlineKeyboardMarkup(row_width=2)
     keyboard.add(*buttons)
-    await call.message.edit_text("Этаж "+ str(floor),misatoMap, reply_markup=keyboard)
+    await call.message.answer("Этаж "+ str(floor), reply_markup=keyboard)
 
-@dp.callback_query_handler(text="-1.1")
-async def text_plus1 (call: types.CallbackQuery, floor=floor):
-    floor = 0
-    buttons = [
-        types.InlineKeyboardButton(text="-1", callback_data="-1"),
-        types.InlineKeyboardButton(text="+1", callback_data="+1"),
-        types.InlineKeyboardButton(text="Подтвердить", callback_data="num_finish0")
-    ]
-    keyboard = types.InlineKeyboardMarkup(row_width=2)
-    keyboard.add(*buttons)
-    # await call.message.edit_media()
-    await call.message.edit_text(f"Этаж + {str(floor)},{misatoMap}", reply_markup=keyboard)
 
-@dp.callback_query_handler(text="-1.2")
-async def text_plus1 (call: types.CallbackQuery, floor=floor):
-    floor = 1
-    buttons = [
-        types.InlineKeyboardButton(text="-1", callback_data="-1.1"),
-        types.InlineKeyboardButton(text="+1", callback_data="+1.1"),
-        types.InlineKeyboardButton(text="Подтвердить", callback_data="num_finish1")
-    ]
-    keyboard = types.InlineKeyboardMarkup(row_width=2)
-    keyboard.add(*buttons)
-    await call.message.edit_text(f"Этаж + {str(floor)}", reply_markup=keyboard)
+# @dp.callback_query_handler(text="+1.1")
+# async def text_plus1(call: types.CallbackQuery, floor=floor):
+#     floor = 2
+#     buttons = [
+#         types.InlineKeyboardButton(text="-1", callback_data="-1.2"),
+#         types.InlineKeyboardButton(text="+1", callback_data="+1.2"),
+#         types.InlineKeyboardButton(text="Подтвердить", callback_data="num_finish2")
+#     ]
+#     keyboard = types.InlineKeyboardMarkup(row_width=2)
+#     keyboard.add(*buttons)
+#     await call.message.edit_text("Этаж "+ str(floor), reply_markup=keyboard)
+#
+# @dp.callback_query_handler(text="+1.2")
+# async def text_plus1(call: types.CallbackQuery, floor=floor):
+#     floor = 3
+#     buttons = [
+#         types.InlineKeyboardButton(text="-1", callback_data="-1.3"),
+#         types.InlineKeyboardButton(text="Подтвердить", callback_data="num_finish3")
+#     ]
+#     keyboard = types.InlineKeyboardMarkup(row_width=2)
+#     keyboard.add(*buttons)
+#     await call.message.edit_text("Этаж "+ str(floor), reply_markup=keyboard)
+#
 
-@dp.callback_query_handler(text="-1.3")
-async def text_plus1 (call: types.CallbackQuery, floor=floor):
-    floor = 2
-    buttons = [
-        types.InlineKeyboardButton(text="-1", callback_data="-1.2"),
-        types.InlineKeyboardButton(text="+1", callback_data="+1.2"),
-        types.InlineKeyboardButton(text="Подтвердить", callback_data="num_finish2")
-    ]
-    keyboard = types.InlineKeyboardMarkup(row_width=2)
-    keyboard.add(*buttons)
-    await call.message.edit_text("Этаж "+ str(floor), reply_markup=keyboard)
-
+#
+# @dp.callback_query_handler(text="-1.1")
+# async def text_plus1(call: types.CallbackQuery, floor=floor):
+#     floor = 0
+#     buttons = [
+#         types.InlineKeyboardButton(text="-1", callback_data="-1"),
+#         types.InlineKeyboardButton(text="+1", callback_data="+1"),
+#         types.InlineKeyboardButton(text="Подтвердить", callback_data="num_finish0")
+#     ]
+#     keyboard = types.InlineKeyboardMarkup(row_width=2)
+#     keyboard.add(*buttons)
+#     # await call.message.edit_media()
+#     await call.message.edit_text(f"Этаж + {str(floor)}", reply_markup=keyboard)
+#
+# @dp.callback_query_handler(text="-1.2")
+#
+# async def text_plus1(call: types.CallbackQuery, floor=floor):
+#     floor = 1
+#     buttons = [
+#         types.InlineKeyboardButton(text="-1", callback_data="-1.1"),
+#         types.InlineKeyboardButton(text="+1", callback_data="+1.1"),
+#         types.InlineKeyboardButton(text="Подтвердить", callback_data="num_finish1")
+#     ]
+#     keyboard = types.InlineKeyboardMarkup(row_width=2)
+#     keyboard.add(*buttons)
+#     await call.message.edit_text(f"Этаж + {str(floor)}", reply_markup=keyboard)
+#
+# @dp.callback_query_handler(text="-1.3")
+# async def text_plus1 (call: types.CallbackQuery, floor=floor):
+#     floor = 2
+#     buttons = [
+#         types.InlineKeyboardButton(text="-1", callback_data="-1.2"),
+#         types.InlineKeyboardButton(text="+1", callback_data="+1.2"),
+#         types.InlineKeyboardButton(text="Подтвердить", callback_data="num_finish2")
+#     ]
+#     keyboard = types.InlineKeyboardMarkup(row_width=2)
+#     keyboard.add(*buttons)
+#     await call.message.edit_text("Этаж "+ str(floor), reply_markup=keyboard)
+#
 
 
 
