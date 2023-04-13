c09e6b89b27a5e5c4afae3196c7b023191ee30f6
birolos <mrbirolos@gmail.com>
2023-04-13 13:17:08 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,10 @@
 import logging
 from aiogram import Bot, Dispatcher, executor, types
-# import asyncio
-# from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, InputFile, CallbackQuery, InputMedia
-# from aiogram.utils.exceptions import BotBlocked
-# from random import randint
-# import aiogram.utils.markdown as fmt
 import sqlite3
 from sqlite3 import Error
-
 from aiogram.types import InputMedia
+
+
 
 conn = None
 try:
@@ -27,6 +23,7 @@ except Error as e:
 conn = None
 
 
+
 g10 = str(*overallname[5]) + f"\n\n" + str(*overallinf[5])
 g1 = str(*overallname[5]) + f"\n\n" + str(*overallinf[5])
 g2 = str(*overallname[6]) + f"\n\n" + str(*overallinf[6])
@@ -38,25 +35,22 @@ e3 = str(*overallname[2]) + f"\n\n" + str(*overallinf[2])
 e4 = str(*overallname[3]) + f"\n\n" + str(*overallinf[3])
 e5 = str(*overallname[4]) + f"\n\n" + str(*overallinf[4])
 jobinf = str(*overallname[10]) + f"\n\n" + str(*overallinf[10])
-
 nervlogo = InputMedia(type="photo", media="https://sun9-49.userapi.com/impg/MMepAmaUCdPhDIC_y-2JP6RspQB_xL5vHWpGQg/plc1NVv_A28.jpg?size=1600x1600&quality=95&sign=5698d100477cdb25d13c33019677b6db&type=album")
 eva00 = InputMedia(type="photo", media="https://sun9-21.userapi.com/impg/v8R4UZa6EJT66ZMP2xkzxTOqCpzSxb1tYI5MyQ/612HPwqWsWw.jpg?size=768x576&quality=95&sign=69fc2b0e75ef5e8b6203cf50768f4d72&type=album")
 eva01 = InputMedia(type="photo", media="https://sun9-15.userapi.com/impg/hkP1tLut5EFwVXVy-1VB5GVk_oEVCRB6S7vbww/vQDe71d44N0.jpg?size=1440x1080&quality=95&sign=f5e5508853deb0c22ea0c6221fa080f8&type=album")
 eva02 = InputMedia(type="photo", media="https://sun9-25.userapi.com/impg/VDTwzaCVoV8RHhjbBNwmF6pP0MrGW6J72CJjUw/aRUZVHcCI_s.jpg?size=640x480&quality=95&sign=bbc63b99be9bdc2c6651604289fe4191&type=album")
 eva03 = InputMedia(type="photo", media="https://sun9-48.userapi.com/impg/lt9eyp9QfyL1GkBKMw7NN_h3ubmDuYVhHF7epQ/bfOO-pGjwGM.jpg?size=875x980&quality=95&sign=e861ceb1bfe09663b5e34f9001c34ef7&type=album")
-
 gendo = InputMedia(type="photo", media="https://sun9-20.userapi.com/impg/hKETV5zbnQCmXhpQVVtx526uKaFY9VM7fAH3Kw/DjDRPLNE05E.jpg?size=1600x2000&quality=95&sign=bb347a40ca95400f0f75e48eb20aecbb&type=album")
 misato = InputMedia(type="photo", media="https://sun9-24.userapi.com/impg/hXpgzWLbLiqxU0MUbhi2C4HQUWTDaF6Q0MuiQA/6Uax8lLkC-o.jpg?size=400x393&quality=95&sign=08df36e2a682a1a642c5a8f59779c7ef&type=album")
 mayaibuki = InputMedia(type="photo", media="https://sun9-79.userapi.com/impg/jgEhpKhbByfROHUSVcfnbJVtgQolflfGSucwgw/QRrFhQegNLQ.jpg?size=1200x675&quality=95&sign=658455c9ee76cfd3130e423132c1f174&type=album")
 ritcuko = InputMedia(type="photo", media="https://sun9-14.userapi.com/impg/QrDG50Dbg_rD7MTcGanA2xubTUAbzGxQMNlK0Q/6Botdvf1Gos.jpg?size=1242x940&quality=95&sign=18a8ad12c0fdb31f87f0eebe325b83e1&type=album")
 makoto = InputMedia(type="photo", media="https://sun9-33.userapi.com/impg/HNndrPpmqRRobAKrOVwAKNqjPeUiFvU9uI1xtA/hl8IbmOgsSg.jpg?size=225x350&quality=95&sign=f217ca844011e91f98ed1ae5c38b8163&type=album")
 magi = InputMedia(type="photo", media="https://sun9-33.userapi.com/impg/_GeklEVDLUs2d_KcO9UST598vUbNudVcnht4Wg/G5oOHoKzaPE.jpg?size=1600x2000&quality=95&sign=35a8896009b9286110e67dbc50512623&type=album")
-
-
 bot = Bot(token="5854619199:AAFO2U5yfHrOm6_9wBZTfudLf_4dgkGizoM")
 dp = Dispatcher(bot)
 logging.basicConfig(level=logging.INFO)
 floor = 0
+
 
 
 @dp.message_handler(commands="start")
@@ -70,6 +64,7 @@ async def start_comms(message: types.Message):
     keyboard = types.InlineKeyboardMarkup()
     keyboard.add(*buttons)
     await message.answer_photo(photo="https://sun9-49.userapi.com/impg/MMepAmaUCdPhDIC_y-2JP6RspQB_xL5vHWpGQg/plc1NVv_A28.jpg?size=1600x1600&quality=95&sign=5698d100477cdb25d13c33019677b6db&type=album", caption=f"Добро пожаловать в onboarding бота Nerv!\nВы можете ознакомиться с продукцией, вашими коллегами, общей инфорамцией о компании и дожлностными обязанностями.",parse_mode=types.ParseMode.HTML, reply_markup=keyboard)
+
 
 @dp.callback_query_handler(text="mainInfo")
 async def main_info(call: types.CallbackQuery):
@@ -96,7 +91,6 @@ async def start_comms(call: types.CallbackQuery):
     await call.message.edit_caption("Добро пожаловать в onboarding бота Nerv!\nВы можете ознакомиться с продукцией, вашими коллегами, общей инфорамцией о компании и дожлностными обязанностями.", parse_mode=types.ParseMode.HTML, reply_markup=keyboard)
 
 
-
 @dp.callback_query_handler(text="goods")
 async def main_goods(call: types.CallbackQuery):
     buttons = [
@@ -107,6 +101,7 @@ async def main_goods(call: types.CallbackQuery):
     keyboard.add(*buttons)
     await call.message.edit_media(eva00, reply_markup=keyboard)
     await call.message.edit_caption(g1, parse_mode=types.ParseMode.HTML, reply_markup=keyboard)
+
 
 @dp.callback_query_handler(text="Good1")
 async def goods1(call: types.CallbackQuery):
@@ -120,6 +115,7 @@ async def goods1(call: types.CallbackQuery):
     await call.message.edit_media(eva01, reply_markup=keyboard)
     await call.message.edit_caption(g2, reply_markup=keyboard)
 
+
 @dp.callback_query_handler(text="Good2")
 async def goods1(call: types.CallbackQuery):
     buttons = [
@@ -131,6 +127,7 @@ async def goods1(call: types.CallbackQuery):
     keyboard.add(*buttons)
     await call.message.edit_media(eva02, reply_markup=keyboard)
     await call.message.edit_caption(g3, reply_markup=keyboard)
+
 
 @dp.callback_query_handler(text="Good3")
 async def goods1(call: types.CallbackQuery):
@@ -144,9 +141,6 @@ async def goods1(call: types.CallbackQuery):
     await call.message.edit_caption(g4, reply_markup=keyboard)
 
 
-#
-#
-
 @dp.callback_query_handler(text="employees")
 async def main_goods(call: types.CallbackQuery):
     buttons = [
@@ -157,6 +151,7 @@ async def main_goods(call: types.CallbackQuery):
     keyboard.add(*buttons)
     await call.message.edit_media(gendo, reply_markup=keyboard)
     await call.message.edit_caption(e1, reply_markup=keyboard)
+
 
 @dp.callback_query_handler(text="empl1")
 async def goods1(call: types.CallbackQuery):
@@ -170,6 +165,7 @@ async def goods1(call: types.CallbackQuery):
     await call.message.edit_media(misato, reply_markup=keyboard)
     await call.message.edit_caption(e2, reply_markup=keyboard)
 
+
 @dp.callback_query_handler(text="empl2")
 async def goods1(call: types.CallbackQuery):
     buttons = [
@@ -181,6 +177,7 @@ async def goods1(call: types.CallbackQuery):
     keyboard.add(*buttons)
     await call.message.edit_media(makoto, reply_markup=keyboard)
     await call.message.edit_caption(e3, reply_markup=keyboard)
+
 
 @dp.callback_query_handler(text="empl3")
 async def goods1(call: types.CallbackQuery):
@@ -194,6 +191,7 @@ async def goods1(call: types.CallbackQuery):
     await call.message.edit_media(ritcuko, reply_markup=keyboard)
     await call.message.edit_caption(e4, reply_markup=keyboard)
 
+
 @dp.callback_query_handler(text="empl4")
 async def goods1(call: types.CallbackQuery):
     buttons = [
@@ -205,6 +203,7 @@ async def goods1(call: types.CallbackQuery):
     await call.message.edit_media(mayaibuki, reply_markup=keyboard)
     await call.message.edit_caption(e5, reply_markup=keyboard)
 
+
 @dp.callback_query_handler(text="job_feats")
 async def jobfeats(call: types.CallbackQuery):
 
@@ -215,7 +214,6 @@ async def jobfeats(call: types.CallbackQuery):
     keyboard.add(*buttons)
     await call.message.edit_media(magi, reply_markup=keyboard)
     await call.message.edit_caption(jobinf, reply_markup=keyboard)
-
 
 
 
