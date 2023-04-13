4286d45614103a36748065552e8b42b4b0c415d3
birolos <mrbirolos@gmail.com>
2023-04-13 13:27:38 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,9 @@
-import logging
+import logging  # тг логгинг в бота
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import InputMedia
 import sqlite3
 from sqlite3 import Error
-from aiogram.types import InputMedia
+
 
 
 
@@ -10,17 +11,17 @@ conn = None
 try:
         conn = sqlite3.connect(".\zxcasuka (1).db")
         cursor = conn.cursor()
-        print("Connection to SQLite DB successful")
-        overallinf = cursor.execute(""" SELECT user_info FROM 'recrd' """)
+        print("Connection to SQLite DB successful")#подключение к бд
+        overallinf = cursor.execute(""" SELECT user_info FROM 'recrd' """) #получение информации из каждой строки бд для дальнейшего использования
         overallinf = overallinf.fetchall()
         overallname = cursor.execute("""SELECT name FROM 'recrd' """)
         overallname = overallname.fetchall()
         overallid = cursor.execute("""SELECT id FROM 'recrd' """)
         overallid = overallid.fetchall()
 except Error as e:
-    print(f"The error '{e}' occurred")
+    print(f"The error '{e}' occurred")#при ошибке подключения или получ. инф из каждой строки бд
 
-conn = None
+conn = None #после всех действий отключаемся от бд
 
 
 
@@ -35,6 +36,7 @@ e3 = str(*overallname[2]) + f"\n\n" + str(*overallinf[2])
 e4 = str(*overallname[3]) + f"\n\n" + str(*overallinf[3])
 e5 = str(*overallname[4]) + f"\n\n" + str(*overallinf[4])
 jobinf = str(*overallname[10]) + f"\n\n" + str(*overallinf[10])
+# структуризация будущего текста. необязательно, но для единого стиля неплохо
 nervlogo = InputMedia(type="photo", media="https://sun9-49.userapi.com/impg/MMepAmaUCdPhDIC_y-2JP6RspQB_xL5vHWpGQg/plc1NVv_A28.jpg?size=1600x1600&quality=95&sign=5698d100477cdb25d13c33019677b6db&type=album")
 eva00 = InputMedia(type="photo", media="https://sun9-21.userapi.com/impg/v8R4UZa6EJT66ZMP2xkzxTOqCpzSxb1tYI5MyQ/612HPwqWsWw.jpg?size=768x576&quality=95&sign=69fc2b0e75ef5e8b6203cf50768f4d72&type=album")
 eva01 = InputMedia(type="photo", media="https://sun9-15.userapi.com/impg/hkP1tLut5EFwVXVy-1VB5GVk_oEVCRB6S7vbww/vQDe71d44N0.jpg?size=1440x1080&quality=95&sign=f5e5508853deb0c22ea0c6221fa080f8&type=album")
@@ -46,25 +48,27 @@ mayaibuki = InputMedia(type="photo", media="https://sun9-79.userapi.com/impg/jgE
 ritcuko = InputMedia(type="photo", media="https://sun9-14.userapi.com/impg/QrDG50Dbg_rD7MTcGanA2xubTUAbzGxQMNlK0Q/6Botdvf1Gos.jpg?size=1242x940&quality=95&sign=18a8ad12c0fdb31f87f0eebe325b83e1&type=album")
 makoto = InputMedia(type="photo", media="https://sun9-33.userapi.com/impg/HNndrPpmqRRobAKrOVwAKNqjPeUiFvU9uI1xtA/hl8IbmOgsSg.jpg?size=225x350&quality=95&sign=f217ca844011e91f98ed1ae5c38b8163&type=album")
 magi = InputMedia(type="photo", media="https://sun9-33.userapi.com/impg/_GeklEVDLUs2d_KcO9UST598vUbNudVcnht4Wg/G5oOHoKzaPE.jpg?size=1600x2000&quality=95&sign=35a8896009b9286110e67dbc50512623&type=album")
+#Создание переменных отвечающие за изображения в сообщениях
 bot = Bot(token="5854619199:AAFO2U5yfHrOm6_9wBZTfudLf_4dgkGizoM")
 dp = Dispatcher(bot)
 logging.basicConfig(level=logging.INFO)
-floor = 0
+#необходимые для бота значения
 
-
-
+#объявляем каждую из функций (сообщений)
+#Каждое сообщение является или овтетом на команду .messag_handler или ответом но нажаитие нкнопки .callback_query_handler
 @dp.message_handler(commands="start")
 async def start_comms(message: types.Message):
     buttons = [
-        types.InlineKeyboardButton(text="О нас", callback_data="mainInfo"),
+        types.InlineKeyboardButton(text="О нас", callback_data="mainInfo"),#callback_data будет триггером для будущих сообщений
         types.InlineKeyboardButton(text="Продукты", callback_data="goods"),
         types.InlineKeyboardButton(text="Сотрудники", callback_data="employees"),
         types.InlineKeyboardButton(text="Обучение должности", callback_data="job_feats")
     ]
-    keyboard = types.InlineKeyboardMarkup()
+    keyboard = types.InlineKeyboardMarkup() # Задаются кнопки, создавая картеж с инф. о самих кнопках
     keyboard.add(*buttons)
     await message.answer_photo(photo="https://sun9-49.userapi.com/impg/MMepAmaUCdPhDIC_y-2JP6RspQB_xL5vHWpGQg/plc1NVv_A28.jpg?size=1600x1600&quality=95&sign=5698d100477cdb25d13c33019677b6db&type=album", caption=f"Добро пожаловать в onboarding бота Nerv!\nВы можете ознакомиться с продукцией, вашими коллегами, общей инфорамцией о компании и дожлностными обязанностями.",parse_mode=types.ParseMode.HTML, reply_markup=keyboard)
-
+# После отправки или имзменения каждого сообщения необходимо добавлять параметр replay_markup
+# parse_mode отвечает за способ модификации текста(\n для пробела строк - HTML)
 
 @dp.callback_query_handler(text="mainInfo")
 async def main_info(call: types.CallbackQuery):
@@ -87,7 +91,7 @@ async def start_comms(call: types.CallbackQuery):
     ]
     keyboard = types.InlineKeyboardMarkup()
     keyboard.add(*buttons)
-    await call.message.edit_media(media=nervlogo, reply_markup=keyboard)
+    await call.message.edit_media(media=nervlogo, reply_markup=keyboard)# Если следующее сообщение подразумевает изменение картинки и текста, то делаем отдельно
     await call.message.edit_caption("Добро пожаловать в onboarding бота Nerv!\nВы можете ознакомиться с продукцией, вашими коллегами, общей инфорамцией о компании и дожлностными обязанностями.", parse_mode=types.ParseMode.HTML, reply_markup=keyboard)
 
 
@@ -215,7 +219,7 @@ async def jobfeats(call: types.CallbackQuery):
     await call.message.edit_media(magi, reply_markup=keyboard)
     await call.message.edit_caption(jobinf, reply_markup=keyboard)
 
-
-
+#запускаем бота
 if __name__ == "__main__":
+
     executor.start_polling(dp, skip_updates=True)
